428ab7b8a994d885146cd953018bb2cd6b8c3742
vascokom <vascokom@gmail.com>
2020-05-03 21:30:44 +0300

    v2

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn_hello.conf /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+sudo gunicorn --bind='0.0.0.0:8080' hello:appHello
+
