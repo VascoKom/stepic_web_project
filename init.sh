fd434dc6547c5496eab156c9d0c416f6127c7a39
vascokom <vascokom@gmail.com>
2020-05-03 22:16:43 +0300

    task_1.9_OK

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo gunicorn --bind='0.0.0.0:8080' hello:appHello
 
