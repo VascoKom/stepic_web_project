38d25602cc118a7359ecfa97e370e413414c3474
vascokom <vascokom@gmail.com>
2020-05-17 10:48:34 +0300

    v3

--- 1_install.sh
+++ 1_install.sh
@@ -1,5 +1,12 @@
+sudo apt update
+sudo apt install python3.5
+sudo ln -sfn /usr/bin/python3.5 /usr/bin/python
+sudo pip install --upgrade pip
+git clone https://github.com/VascoKom/stepic_web_project /home/box/web
+
+cd /home/box/web
 virtualenv -p python3 env
-source myvenv/bin/activate
+source env/bin/activate
 pip install --upgrade pip
 pip install django==2.2.5
 pip install gunicorn==19.9.0
---
+++ gunicorn_cfg.py
@@ -0,0 +1,7 @@
+mode='django'
+working_dir='/home/box/web/ask',
+bind='0.0.0.0:8000'
+workers=2
+timeout=15
+loglevel='debug'
+
