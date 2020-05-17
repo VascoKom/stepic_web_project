5a6175370c614a466fbf84da90305b263e9c7158
vascokom <vascokom@gmail.com>
2020-05-17 10:51:15 +0300

    v3

--- 1_install.sh
+++ 1_install.sh
@@ -2,7 +2,6 @@ sudo apt update
 sudo apt install python3.5
 sudo ln -sfn /usr/bin/python3.5 /usr/bin/python
 sudo pip install --upgrade pip
-git clone https://github.com/VascoKom/stepic_web_project /home/box/web
 
 cd /home/box/web
 virtualenv -p python3 env
