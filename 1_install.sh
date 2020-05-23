685142edb376f9368336bb9e3363aed9691384f9
vascokom <vascokom@gmail.com>
2020-05-23 21:36:09 +0300

    v3

--- 1_install.sh
+++ 1_install.sh
@@ -3,6 +3,10 @@ sudo apt install python3.5
 sudo ln -sfn /usr/bin/python3.5 /usr/bin/python
 sudo pip install --upgrade pip
 
+pip install virtualenv
+apt-get install python3-venv
+#python3 -m venv env
+
 cd /home/box/web
 virtualenv -p python3 env
 source env/bin/activate
